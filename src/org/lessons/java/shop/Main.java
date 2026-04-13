09d5deace0a16cb618be1f854eb805d15b3ab48a
Maurizio Piroddi <x.hacker.hiphop99@gmail.com>
2026-04-13 16:14:36 +0200

    Creazione di un paio di prodotti di esempio, creazione metodi per stampare in pagina i valori contenuti negli oggetti

--- src/org/lessons/java/shop/Main.java
+++ src/org/lessons/java/shop/Main.java
@@ -1,14 +1,31 @@
 package org.lessons.java.shop;
 
+import java.math.BigDecimal;
+
 public class Main {
 
     public static void main(String[] args) {
 
         Prodotto saponeDiMarsiglia = new Prodotto("Sapone di Marsiglia", "Un sapone che pulisce benissimo.",
-                22.10f, 1.22f);
+                new BigDecimal(8.2), new BigDecimal(0.22));
+        Prodotto doritosChili = new Prodotto("Doritos gusto chili",
+                "Tortillas fritte, tossiche ma veramente buonissime!",
+                new BigDecimal(2.10), new BigDecimal(0.22));
+        Prodotto oreoClassic = new Prodotto("Oreo gusto classico",
+                "Un biscotto al cacao con dentro vaglia, sempre abbastanza tossici ma vabbè.",
+                new BigDecimal(1.4), new BigDecimal(0.22));
 
-        System.out.println(saponeDiMarsiglia.nome);
-        System.out.println(saponeDiMarsiglia.codice);
+        System.out.println(saponeDiMarsiglia.getFullName());
+        System.out.println(saponeDiMarsiglia.getPrice());
+        System.out.println(saponeDiMarsiglia.getPriceWithIva());
+
+        System.out.println(doritosChili.getFullName());
+        System.out.println(doritosChili.getPrice());
+        System.out.println(doritosChili.getPriceWithIva());
+
+        System.out.println(oreoClassic.getFullName());
+        System.out.println(oreoClassic.getPrice());
+        System.out.println(oreoClassic.getPriceWithIva());
 
     }
 
--- src/org/lessons/java/shop/Prodotto.java
+++ src/org/lessons/java/shop/Prodotto.java
@@ -1,16 +1,18 @@
 package org.lessons.java.shop;
 
 import java.util.Random;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class Prodotto {
 
     public int codice;
     public String nome;
     public String descrizione;
-    public float prezzo;
-    public float iva;
+    public BigDecimal prezzo;
+    public BigDecimal iva;
 
-    public Prodotto(String nome, String descrizione, float prezzo, float iva) {
+    public Prodotto(String nome, String descrizione, BigDecimal prezzo, BigDecimal iva) {
 
         Random randomNumber = new Random();
 
@@ -19,6 +21,18 @@ public class Prodotto {
         this.descrizione = descrizione;
         this.prezzo = prezzo;
         this.iva = iva;
+    }
+
+    public BigDecimal getPrice() {
+        return this.prezzo.setScale(2, RoundingMode.DOWN);
+    }
+
+    public BigDecimal getPriceWithIva() {
+        return this.prezzo.add(prezzo.multiply(iva)).setScale(2, RoundingMode.DOWN);
+    }
+
+    public String getFullName() {
+        return this.codice + "-" + this.nome;
     }
 
 }
